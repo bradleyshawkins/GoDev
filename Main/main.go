77448b41713309e0edcb6e8fad825854c778ff76
Brad Hawkins <Brad Hawkins>
2016-10-30 23:24:58 -0600

    adding another file

--- Main/main.go
+++ Main/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/bradleyshawkins/GoDev/MyStruct"
+	"github.com/bradleyshawkins/stringutil"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(stringutil.Reserse("Hello, World!"))
+	structs := MyStruct.ExportStruct{Value: 7}
+	fmt.Println(structs.PrintValue())
+	fmt.Println(MyStruct.PrintVal())
 }
